1aadd8c7adf2b5e41066e74e09215780c5354e64
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-04-30 10:19:49 +0530

    update

--- src/Components/Ratings.js
+++ src/Components/Ratings.js
@@ -5,9 +5,11 @@ export const Ratings = ({ rating, handleRatingClick }) => {
   return (
     <span>
       {
-        [...Array(5)].map((_, i) => {
-          return rating >= i + 1 ? <AiFillStar onClick={() => handleRatingClick(i + 1)} style={{ height: "13px" }} /> : <AiOutlineStar onClick={() => handleRatingClick(i + 1)} style={{ height: "13px" }} />
-        })
+        [...Array(5)].map((_, i) => <span>
+          {
+            rating >= i + 1 ? <AiFillStar onClick={() => handleRatingClick(i + 1)} style={{ height: "13px" }} /> : <AiOutlineStar onClick={() => handleRatingClick(i + 1)} style={{ height: "13px" }} />
+          }
+        </span>)
       }
     </span>
   )
